22fdbe33f1a44abac6cac71abcaf7f7fd5b69b5c
JateenSingh <jitz303@gmail.com>
2018-03-19 16:32:57 +0200

    Added implementation and webservices for the System jobs

---
+++ src/aoc/Harlequin/DAOs/JobDAO.java
@@ -0,0 +1,82 @@
+package aoc.Harlequin.DAOs;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+
+
+
+import aoc.Harlequin.OBJs.SystemClient;
+import aoc.Harlequin.OBJs.SystemJob;
+import aoc.Harlequin.util.HibernateUtil;
+
+public class JobDAO extends HarlequinDAO {
+	
+	public SystemJob getJobInfoById(int id)
+	{
+		
+		
+		Session session = this.getSession();
+
+		HibernateUtil.beginTransaction();
+	   
+		
+		SystemJob jobs = session.get(SystemJob.class, id);
+		
+		session.clear(); // ADDED 170302
+		session.flush();
+		session.close();
+	   
+		
+		return jobs;
+		
+		
+	}
+	
+	public List<SystemJob> ReadAllJobs()
+	{
+		Session session = this.getSession();
+		HibernateUtil.beginTransaction();
+		
+		
+		
+		Query query = session.createQuery("from SystemJob");
+		List<SystemJob> Job = query.list();
+		
+		session.clear(); // ADDED 170302
+		session.flush();
+		session.close();
+		
+		return Job;
+		
+		
+		
+	}
+	
+	public void AddJobInformation( String jobName, String jobCode, String jobClientName,String jobDetails, String jobComments)
+	{
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		
+		session.beginTransaction();
+		
+		
+		
+		SystemJob job = new SystemJob();
+		
+		job.setJobCode(jobCode);
+		job.setJobClientName(jobClientName);
+		job.setJobDetails(jobDetails);
+		job.setJobComments(jobComments);
+		
+		session.save(job);
+				
+		session.getTransaction().commit();
+		
+		session.close();
+		
+	}
+
+}
---
+++ src/aoc/Harlequin/Services/JobService.java
@@ -0,0 +1,116 @@
+package aoc.Harlequin.Services;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import aoc.Harlequin.DAOs.ClientDAO;
+import aoc.Harlequin.DAOs.JobDAO;
+import aoc.Harlequin.DAOs.SystemUserDAO;
+import aoc.Harlequin.OBJs.SystemClient;
+import aoc.Harlequin.OBJs.SystemJob;
+import aoc.Harlequin.OBJs.SystemUser;
+
+
+
+
+@Path("Job")
+public class JobService {
+	
+	@Path("/GetJobInfo")
+	@GET
+	@Produces("text/plain")
+	public String GET( ) throws Exception
+	{
+		
+		JobDAO Object  = new JobDAO();
+		SystemJob Job  = Object.getJobInfoById(1);
+		
+		JSONObject jsonObject = new JSONObject();
+		
+		jsonObject.put("Job_Id", Job.getJobId());
+		jsonObject.put("Job_Name", Job.getJobName());
+		jsonObject.put("Job_Code", Job.getJobCode());
+		jsonObject.put("Job_Client_Name", Job.getJobClientName());
+		jsonObject.put("Job_Details", Job.getJobDetails());		
+		jsonObject.put("Job_Comments", Job.getJobComments());
+		
+		
+		
+		System.out.println(jsonObject.toString());
+	    
+		return jsonObject.toString();
+	}
+	
+	@Path("/GetAllJobs")
+	@GET
+	@Produces("text/plain")
+	public String GETClients( ) throws Exception
+	{
+		
+		JobDAO Object  = new JobDAO();
+		
+		List<SystemJob> Jobs = Object.ReadAllJobs();
+		
+		JSONArray JsonArray = new JSONArray();
+		
+		for(int i = 0; i < Jobs.size();i++)
+		{
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("Job_Id", Jobs.get(i).getJobId());
+			jsonObject.put("Job_Name", Jobs.get(i).getJobName());
+			jsonObject.put("Job_Code", Jobs.get(i).getJobCode());
+			jsonObject.put("Job_Client_Name", Jobs.get(i).getJobClientName());
+			jsonObject.put("Job_Details", Jobs.get(i).getJobDetails());		
+			jsonObject.put("Job_Comments", Jobs.get(i).getJobComments());
+			
+			
+			JsonArray.put(jsonObject);
+		}
+		
+		
+		
+		
+		
+		
+		System.out.println(JsonArray.toString());
+	    
+		return JsonArray.toString();
+	}
+	
+	
+	
+	
+	@Path("/SaveJobInfo")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces("text/plain")
+	public  String create(String jsonTextObject) throws JSONException
+	{
+		
+		System.out.println(jsonTextObject);
+		JSONObject r = new JSONObject(jsonTextObject);	
+	
+		
+		System.out.println("WRITING TO DATABASE:"+ r.getString("Client_Name"));
+			
+		JobDAO Object  = new JobDAO();
+		Object.AddJobInformation(r.getString("Job_Name"), r.getString("Job_Code"), r.getString("Job_Client_Name"), r.getString("Job_Details"), r.getString("Job_Comments"));
+		return "Sucessful";	
+		
+			
+	}
+	
+	
+	
+
+}
