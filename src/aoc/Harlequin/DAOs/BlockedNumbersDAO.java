b059a038e6e8b2a8239255a8733f2dc79f09f852
JateenSingh <jitz303@gmail.com>
2018-07-16 13:19:53 +0200

    Added new feature to add blocked numbers to the system

---
+++ src/aoc/Harlequin/DAOs/BlockedNumbersDAO.java
@@ -0,0 +1,96 @@
+package aoc.Harlequin.DAOs;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import aoc.Harlequin.OBJs.BlockedNumberList;
+import aoc.Harlequin.OBJs.SystemClient;
+import aoc.Harlequin.OBJs.SystemUser;
+import aoc.Harlequin.util.HibernateUtil;
+
+public class BlockedNumbersDAO extends HarlequinDAO {
+
+	
+	
+	
+	public List<BlockedNumberList> ReadAllBlockedNumbers()
+	{
+		Session session = this.getSession();
+		HibernateUtil.beginTransaction();
+		
+		
+		
+		Query query = session.createQuery("from BlockedNumberList");
+		List<BlockedNumberList> Client = query.list();
+		
+		session.clear(); // ADDED 170302
+		session.flush();
+		session.close();
+		
+		return Client;
+		
+		
+		
+	}
+	
+	
+	public void delete(Object entity) 
+	{
+		  Session hibernateSession = this.getSession(); 
+		  
+		  HibernateUtil.beginTransaction();
+		        hibernateSession.delete(entity);
+		        hibernateSession.flush();
+		  HibernateUtil.commitTransaction();
+	}
+	
+	public BlockedNumberList getBlockedNumberInfoById(int id)
+	{
+		
+		
+		Session session = this.getSession();
+
+		HibernateUtil.beginTransaction();
+	   
+		
+		BlockedNumberList BlockedNumber = session.get(BlockedNumberList.class, id);
+		
+		
+		session.close();
+	   
+		
+		return BlockedNumber;
+		
+		
+	}
+	
+	
+	public void AddBlockedNumberInfo(String CellNumber, String Date_Blocked)
+	{
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		
+		session.beginTransaction();
+		
+		
+		
+		BlockedNumberList BlockedNumber = new BlockedNumberList();
+		
+		BlockedNumber.setCellNumber(CellNumber);
+		BlockedNumber.setDateBlocked(Date_Blocked);
+		
+		
+		session.save(BlockedNumber);
+				
+		session.getTransaction().commit();
+		
+		
+		
+		session.close();
+
+		
+	}
+	
+}
---
+++ src/aoc/Harlequin/Services/BlockedNumbersService.java
@@ -0,0 +1,100 @@
+package aoc.Harlequin.Services;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import aoc.Harlequin.DAOs.BlockedNumbersDAO;
+
+import aoc.Harlequin.OBJs.BlockedNumberList;
+
+@Path("BlockedCellNumbers")
+public class BlockedNumbersService 
+{
+	
+	@Path("/ReadAllBlockedNumbers")
+	@GET
+	@Produces("text/plain")
+	public String GetAllBlockedNumbers() throws Exception
+	{
+	    BlockedNumbersDAO Object  = new BlockedNumbersDAO();
+		
+		List<BlockedNumberList> BlockedNumbers = Object.ReadAllBlockedNumbers();
+		
+		JSONArray JsonArray = new JSONArray();
+		
+		for(int i = 0; i < BlockedNumbers.size();i++)
+		{
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("idBlockedNumberList", BlockedNumbers.get(i).getIdBlockedNumberList());
+			jsonObject.put("cellNumber", BlockedNumbers.get(i).getCellNumber());
+			jsonObject.put("dateBlocked", BlockedNumbers.get(i).getDateBlocked());
+			
+			
+			
+			JsonArray.put(jsonObject);
+		}
+		
+		
+		
+		
+		
+		
+	
+		
+		
+	    
+		return JsonArray.toString();
+	}
+	
+	
+	@Path("/DeleteNumber/{Id}")
+	@GET
+	@Produces("text/plain")
+	public String Delete( @PathParam("Id") int Id) throws Exception
+	{
+		
+		BlockedNumbersDAO Object  = new BlockedNumbersDAO();
+		
+		Object.delete(Object.getBlockedNumberInfoById(Id));
+	    
+		return "Successful";
+	}
+	
+	
+	@Path("/AddBlockedNumber")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces("text/plain")
+	public  String create(String jsonTextObject) throws JSONException
+	{
+		
+		System.out.println(jsonTextObject);
+		JSONObject r = new JSONObject(jsonTextObject);	
+	
+		
+		System.out.println("WRITING TO DATABASE:"+ r.getString("CellNumber"));
+			
+		BlockedNumbersDAO Object  = new BlockedNumbersDAO();
+		
+		Object.AddBlockedNumberInfo(r.getString("CellNumber"), r.getString("Date_Blocked"));
+		
+		return "Sucessful";	
+		
+			
+	}
+	
+
+}
+
